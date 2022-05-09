4d3c21bc7db4bf142080d7aa4b6c0988bb912a9b
Pablo Dellacassa <pdellacassa@gmail.com>
2022-05-09 16:34:08 +0200

    Updated

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,23 +3,25 @@
 const { Router } = require("express");
 const router = new Router();
 
-const bcryptjs = require("bcryptjs");
-const saltRounds = 10;
+const bcrypt = require("bcrypt");
+const saltRounds = 5;
 
 const User = require("../models/User.model");
+const res = require("express/lib/response");
 
 // GET route ==> to display the signup form to users
-router.get("/signup", (req, res) => res.render("auth/signup"));
-
-// POST route ==> to process form data
-router.post("/signup", (req, res, next) => {
-  // console.log("The form data: ", req.body);
+router
+  .route("/signup")
+  .get((req, res) => res.render("auth/signup"))
+  // POST route ==> to process form data
+  .post((req, res, next) => {
+    // console.log("The form data: ", req.body);
 
   const { username, email, password } = req.body;
 
-  bcryptjs
+  bcrypt
     .genSalt(saltRounds)
-    .then((salt) => bcryptjs.hash(password, salt))
+    .then((salt) => bcrypt.hash(password, salt))
     .then((hashedPassword) => {
       return User.create({
         // username: username
@@ -28,15 +30,39 @@ router.post("/signup", (req, res, next) => {
         // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
       });
     })
     .then((userFromDB) => {
       // console.log("Newly created user is: ", userFromDB);
-      res.redirect("/userProfile");
+      res.redirect("/");
     })
     .catch((error) => next(error));
 });
+
+router
+  .route("/login")
+  .get((req, res) => res.render("auth/login"))
+  .post((req, res) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          res.render("auth/login", { errorMessage: "Wrong credentials!" });
+          return;
+        } else {
+          if (bcrypt.compareSync(password, user.passwordHash)) {
+            req.session.currentUser = user;
+            res.redirect("/"); // redirect to wherever you want
+            return;
+          } else {
+            res.render("auth/login", { errorMessage: "Wrong credentials!" });
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  });
 
 router.get("/userProfile", (req, res) => res.render("users/user-profile"));
 
